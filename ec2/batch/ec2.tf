ce9132cb9c948aa4f6c50c7d010e0ee8ee8107a0
wuyang <yangwu1129@gmail.com>
2025-10-16 16:26:22 +0900

    ec2 update

--- ec2/batch/ec2.tf
+++ ec2/batch/ec2.tf
@@ -1,8 +1,10 @@
 # AWS EC2 Instance Terraform Module
 # Bastion Host - EC2 Instance that will be created in VPC Public Subnet
 module "ec2_complete" {
-  source  = "terraform-aws-modules/ec2-instance/aws" 
-  version = "6.1.2"  
+  # source  = "terraform-aws-modules/ec2-instance/aws" 
+  # version = "6.1.2" 
+  # sourceローカル化 
+  source  = "./modules/ec2"
 
   name                   = local.name
   instance_type          = var.instance_type

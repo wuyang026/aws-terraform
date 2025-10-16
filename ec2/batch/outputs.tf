aa385347e9edcacb836b5428f3df779810e18ab3
wuyang <yangwu1129@gmail.com>
2025-10-16 18:50:36 +0900

    ec2 batch update

--- ec2/batch/outputs.tf
+++ ec2/batch/outputs.tf
@@ -1,15 +1,15 @@
 # EC2 Complete
-output "ec2_complete_id" {
+output "ec2_batch_id" {
   description = "The ID of the instance"
-  value       = module.ec2_complete.id
+  value       = module.ec2_batch.id
 }
 
-output "ec2_complete_arn" {
+output "ec2_batch_arn" {
   description = "The ARN of the instance"
-  value       = module.ec2_complete.arn
+  value       = module.ec2_batch.arn
 }
 
-output "ec2_complete_instance_state" {
+output "ec2_batch_instance_state" {
   description = "The state of the instance. One of: `pending`, `running`, `shutting-down`, `terminated`, `stopping`, `stopped`"
-  value       = module.ec2_complete.instance_state
+  value       = module.ec2_batch.instance_state
 }
